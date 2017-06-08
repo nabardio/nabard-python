b83b276d7fef0165733b465c3a0ea54dfb97093c
mehdy <mehdy.khoshnoody@gmail.com>
2017-06-08 13:11:01 +0430

    fixed robot id var name

--- battlefield/engine/robot.py
+++ battlefield/engine/robot.py
@@ -3,13 +3,13 @@
 
 class Robot(object):
     """
-    Robot is a simple class for the game engine to avoid using the original 
-    robot class. it handles the the robot process, score, and socket to the 
+    Robot is a simple class for the game engine to avoid using the original
+    robot class. it handles the the robot process, score, and socket to the
     process.
     """
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, id):
+        self.id = id
         self.score = 0
         self.sock = None
         self.process = None
