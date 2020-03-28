29fa9a7b1cff4aa0f3f34d1fbef2a89fc96fc5d2
Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>
2020-03-28 14:47:52 +0430

    Update setup.py and dependency management

--- setup.py
+++ setup.py
@@ -1,24 +1,21 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name='Battlefield',
-    version='1.0.3',
+    name="nabard",
+    version="0.1.0",
     packages=find_packages(),
-    install_requires=['pika'],
+    install_requires=[],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
     ],
-    keywords='robot game battlefield',
-    url='https://github.com/tehpug/Battlefield',
-    license='GPLv3',
-    author='Mehdy Khoshnoody',
-    author_email='mehdy.khoshnoody@gmail.com',
-    description='Utilities to create games and robots for Battlefield')
+    keywords="robot game code nabard",
+    url="https://github.com/nabardio/nabard-python",
+    license="GPLv3",
+    author="Mehdy Khoshnoody",
+    author_email="mehdy.khoshnoody@gmail.com",
+    description="Utilities to create games and robots for https://nabard.io",
+)
